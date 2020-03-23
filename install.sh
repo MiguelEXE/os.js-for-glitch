458feb35f441ef64cbb0332b2fde08bc5f237ddd
MiguelEXE <52385139+MiguelEXE@users.noreply.github.com>
2020-03-23 01:44:09 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ git clone https://github.com/MiguelEXE/start-os-js-glitch.git
 mv start-os-js-glitch/runIt.sh .
 rm -rf start-os-js-glitch
 
+chmod +x upgrade.sh
 refresh
-clear
+#clear
 echo Check you application logs
