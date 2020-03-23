db8464fe4946662c213541287eac79b5550d6f9b
MiguelEXE <52385139+MiguelEXE@users.noreply.github.com>
2020-03-23 01:42:57 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,18 +12,10 @@ clear
 cd ..
 
 git clone https://github.com/MiguelEXE/start-os-js-glitch.git
-git clone https://github.com/jakeonrails/git-notify
-
-mv git-notify gitnotify-folder
-mv git-notify-folder/git-notify .
-mv git-notify updater
 
 mv start-os-js-glitch/runIt.sh .
 rm -rf start-os-js-glitch
-rm -rf git-notify-folder
-
-ls
 
 refresh
-#clear
+clear
 echo Check you application logs
