52289285360cfe7721018351eceb40de297d2dd2
MiguelEXE <52385139+MiguelEXE@users.noreply.github.com>
2020-03-23 01:32:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,10 @@ cd ..
 git clone https://github.com/MiguelEXE/start-os-js-glitch.git
 git clone https://github.com/jakeonrails/git-notify
 
-mv git-notify/git-notify ..
+mv git-notify/git-notify .
 mv git-notify auto-updater
 
-mv start-os-js-glitch/runIt.sh ..
+mv start-os-js-glitch/runIt.sh .
 rm -rf start-os-js-glitch
 rm -rf git-notify
 
